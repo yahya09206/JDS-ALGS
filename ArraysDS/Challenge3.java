8af86798ebca8808cef750ef47d3457fabdc29ef
Yahya Hussein <yahyahussein91@gmail.com>
2021-03-31 15:59:59 -0700

    tweaking

--- ArraysDS/Challenge3.java
+++ ArraysDS/Challenge3.java
@@ -1,10 +1,10 @@
 import java.util.*;
 public class Challenge3 {
 	public static void main(String[] args){
-		int[] array = {1, 5, 3, 7, 11, 9, 15};
-		System.out.println("Array = " + Arrays.toString(array));
-		reverse(array);
-		System.out.println("Reversed Array = " + Arrays.toString(array));
+		int[] newArray = {1, 5, 3, 7, 11, 9, 15};
+		System.out.println("Array = " + Arrays.toString(newArray));
+		reverse(newArray);
+		System.out.println("Reversed Array = " + Arrays.toString(newArray));
 	}
 	private static void reverse(int[] array){
 		int maxIndex = array.length - 1;
