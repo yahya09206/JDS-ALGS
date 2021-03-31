b7fb4b81de474d9a9b0ea7d67daa6685329127b9
Yahya Hussein <yahyahussein91@gmail.com>
2021-03-30 21:26:32 -0700

    added methods to be used for challenge

--- ArraysDS/Challenge.java
+++ ArraysDS/Challenge.java
@@ -4,4 +4,15 @@ public class Challenge {
 	public static void main(String[] args){
 		
 	}
+	public static int[] getIntegers(int number){
+
+	}
+
+	public static void printArray(){
+
+	}
+
+	public static int[] sortArray(){
+		
+	}
 }
