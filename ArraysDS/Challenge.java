8fa6174f3e1465be279d08e5280a1e74ee78a9a7
Yahya Hussein <yahyahussein91@gmail.com>
2021-03-31 14:11:45 -0700

    done with sorted method with help

--- ArraysDS/Challenge.java
+++ ArraysDS/Challenge.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 public class Challenge {
 	private static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args){
-		
+		int[] myIntegers = getIntegers(5);
 	}
 	public static int[] getIntegers(int numbers){
-		System.out.println("Enter " + numbers + " integer values.\r");
 		int[] values = new int[numbers];
+		System.out.println("Enter " + numbers + " integer values.\r");
 		//Loop through numbers and return int
 		for(int i = 0; i < values.length; i++){
 			values[i] = scanner.nextInt();
@@ -15,11 +16,29 @@ public class Challenge {
 		return values;
 	}
 
-	public static void printArray(){
-
+	public static void printArray(int[] array){
+		for(int i = 0; i < array.length; i++){
+			System.out.println("Element " + i + ", value is " + array[i]);
+		}
 	}
 
-	public static int[] sortArray(){
-		
+	public static int[] sortArray(int[] array){
+		int[] sortedArray = new int[array.length];
+		for(int i = 0; i < array.length; i++){
+			sortedArray[i] = array[i];
+		}
+		boolean flag = true;
+		int temp;
+		while(flag){
+			flag = false;
+			for(int i = 0; i < sortedArray.length - 1; i++){
+				if(sortedArray[i] < sortedArray[i + i]){
+					temp = sortedArray[i];
+					sortedArray[i] = sortedArray[i + 1];
+					sortedArray[i + 1] = temp;
+					flag = true;
+				}
+			}
+		}
 	}
 }
