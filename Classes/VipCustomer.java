6303821c64645458339fb4a27aa7ecfbe019319a
Yahya Hussein <yahyahussein91@gmail.com>
2021-04-15 18:27:02 -0700

    set up constructor with 1 default value and two non default values

--- Classes/VipCustomer.java
+++ Classes/VipCustomer.java
@@ -8,4 +8,8 @@ public VipCustomer {
 		this(123400.00, "johnny", "johnny@email.com");
 		System.out.println("Empty Constructor Called.");
 	}
+
+	public VipCustomer(double creditLimit, double name){
+		this("johnny@email.com", creditLimit, name);
+	}
 }
