aadc4bd018eaac77a1a8d5aed5fa41dbacce1d31
Yahya Hussein <yahyahussein91@gmail.com>
2021-04-17 16:29:13 -0700

    done with carpet class

--- Classes/Carpet.java
+++ Classes/Carpet.java
@@ -1,6 +1,18 @@
 public class Carpet {
 
-	privte double cost;
+	private double cost;
+
+	public Class(double cost){
+		if(cost < 0){
+			this.cost = 0;
+		}else {
+			this.cost = cost;
+		}
+	}
+
+	public double getCost(){
+		return this.cost;
+	}
 
 	
 
