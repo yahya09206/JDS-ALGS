165cfc915b80a895fe3801bcd2f18a45ed56f53b
Yahya Hussein <yahyahussein91@gmail.com>
2021-04-17 16:26:33 -0700

    created second class of carper

--- Classes/Floor.java
+++ Classes/Floor.java
@@ -1,7 +1,23 @@
 public class Floor {
 
 	private double width;
-	private double height;
+	private double length;
 
-	
+	public Floor(double width, double length){
+		if(width < 0){
+			this.width = 0;
+		}else {
+			this.width = width;
+		}
+		if(length < 0){
+			this.length = 0;
+		}else {
+			this.length = length;
+		}
+	}
+
+	public double getArea(){
+		return width * length
+	}
+
 }
