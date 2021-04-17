b3b3386785683e4a07f21f906ac4445f8ce6460c
Yahya Hussein <yahyahussein91@gmail.com>
2021-04-17 16:22:08 -0700

    added instance methods. Challenge completed

--- Classes/Point.java
+++ Classes/Point.java
@@ -20,10 +20,22 @@ public class Point {
 	}
 
 	public void setX(int x){
-		return this.x;
+		this.x = x;;
 	}
 
-	public void sexY(int y){
-		return this.y;
+	public void setY(int y){
+		this.y = y;
 	}
+
+	public double distance() {
+        return Math.sqrt((getX()*getX()) + (getY()*getY()));
+    }
+ 
+    public double distance(int x, int y) {
+        return Math.sqrt((x-getX())*(x-getX()) + (y-getY())*(y-getY()));
+    }
+ 
+    public double distance(Point point) {
+        return distance(point.x, point.y);
+    }
 }
