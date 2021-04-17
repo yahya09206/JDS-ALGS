b0900c8f1716ec32f49995601a6bf668c2a0fcc2
Yahya Hussein <yahyahussein91@gmail.com>
2021-04-17 16:08:14 -0700

    set up constructor with params and default one

--- Classes/Wall.java
+++ Classes/Wall.java
@@ -1,4 +1,21 @@
 public class Wall {
 	private double width;
 	private double height;
+
+	public Wall(){
+
+	}
+
+	public Wall(double width, double height){
+		if(width < 0){
+			this.width = 0;
+		}else {
+			this.width = width;
+		}
+		if(height < 0){
+			this.height = 0;
+		}else {
+			this.height = height;
+		}
+	}
 }
