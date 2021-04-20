8112bcd02860149133b02c6aa547631cad708fc1
Yahya Hussein <yahyahussein91@gmail.com>
2021-04-20 16:51:55 -0700

    added more movement methods for dog class

--- InheritanceAndInterfaces/Animal.java
+++ InheritanceAndInterfaces/Animal.java
@@ -18,8 +18,8 @@ public class Animal {
 		System.out.println("Animal.eat() called.");
 	}
 
-	public void move(){
-
+	public void move(int speed){
+		System.out.println("Animal is moving at speed " + speed);
 	}
 
 	public String getName(){
