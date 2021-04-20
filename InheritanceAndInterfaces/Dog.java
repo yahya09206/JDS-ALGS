9782aff0ef17aac927d78f22ccc9a5bcfa44e3f3
Yahya Hussein <yahyahussein91@gmail.com>
2021-04-20 16:43:47 -0700

    saved fields from dog class

--- InheritanceAndInterfaces/Dog.java
+++ InheritanceAndInterfaces/Dog.java
@@ -6,8 +6,13 @@ public class Dog extends Animal {
 	private int teeth;
 	private String coat;
 
-	public Dog(String name, int size, int weight){
+	public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat){
 		//Super calls constructor from class we are inherting from
 		super(name, 1, 1, size, weight);
+		this.eyes = eyes;
+		this.legs = legs;
+		this.tail = tail;
+		this.teeth = teeth;
+		this.coat = coat;
 	}
 }
