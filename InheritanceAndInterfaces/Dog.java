1ec637edf6b4c147ab2435414064357e4f3f9265
Yahya Hussein <yahyahussein91@gmail.com>
2021-04-20 16:39:33 -0700

    added fields for dog class

--- InheritanceAndInterfaces/Dog.java
+++ InheritanceAndInterfaces/Dog.java
@@ -1,6 +1,13 @@
 public class Dog extends Animal {
 
+	private int eyes;
+	private int legs;
+	private int tail;
+	private int teeth;
+	private String coat;
+	
 	public Dog(String name, int brain, int body, int size, int weight){
+		//Super calls constructor from class we are inherting from
 		super(name, brain, body, size, weight);
 	}
 }
