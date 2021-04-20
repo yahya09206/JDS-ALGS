9513c3da7048b55871d98b2bf5ae096b21ed714f
Yahya Hussein <yahyahussein91@gmail.com>
2021-04-20 16:45:15 -0700

    instantiated new instances of both classes

--- InheritanceAndInterfaces/Main.java
+++ InheritanceAndInterfaces/Main.java
@@ -1,5 +1,8 @@
 public class Main {
 	public static void main(String[] args){
-		
+
+		Animal animal = new Animal("Bit", 1, 1, 5, 5);
+
+		Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
 	}
 }
