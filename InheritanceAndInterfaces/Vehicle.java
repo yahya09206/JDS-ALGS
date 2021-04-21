1419b8100961a0192fd298373d03f78bc5bf7e11
Yahya Hussein <yahyahussein91@gmail.com>
2021-04-20 18:21:41 -0700

    added getters and methods for direction and velocity

--- InheritanceAndInterfaces/Vehicle.java
+++ InheritanceAndInterfaces/Vehicle.java
@@ -13,4 +13,24 @@ public class Vehicle {
 		this.currentVelocity = 0;
 		this.currentDirection = 0;
 	}
+
+	public void steer(int direction){
+		this.currentDirection = direction;
+		System.out.println("Vehicle.steer(): Steering at " + currentDirection + " degrees.");
+	}
+
+	public void move(int velocity, int direction){
+		currentVelocity = velocity;
+		currentDirection = direction;
+		System.out.println("Vehicle.move(): Moving at " + currentVelocity + " in direction " + currentDirection);
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public String getSize(){
+		return this.size;
+	}
+
 }
