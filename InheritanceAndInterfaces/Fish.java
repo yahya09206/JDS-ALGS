f7b7441647b303f64551ba3fec9c6918940ecb19
Yahya Hussein <yahyahussein91@gmail.com>
2021-04-20 17:37:42 -0700

    created constructor

--- InheritanceAndInterfaces/Fish.java
+++ InheritanceAndInterfaces/Fish.java
@@ -1,6 +1,15 @@
-public class Fish {
+public class Fish extends Animal {
 
 	private int gills;
 	private int eyes;
 	private int fins;
+
+	public Fish(String name, int size, int weight, int gills, int eyes, int fins){
+		super(name, 1, 1, size, weight);
+		this.gills = gills;
+		this.eyes = eyes;
+		this.fins = fins;
+	}
+
+	
 }
