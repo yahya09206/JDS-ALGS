c2cdb5c3b2bc9bd205b7fabe7960948f20b18cb3
Yahya Hussein <yahyahussein91@gmail.com>
2021-04-20 18:19:08 -0700

    created fields and constructor for vehicle class

--- InheritanceAndInterfaces/Vehicle.java
+++ InheritanceAndInterfaces/Vehicle.java
@@ -1,3 +1,16 @@
 public class Vehicle {
-	
+
+	private String name;
+	private String size;
+
+	private int currentVelocity;
+	private int currentDirection;
+
+	public Vehicle(String name, String size){
+		this.name = name;
+		this.size = size;
+
+		this.currentVelocity = 0;
+		this.currentDirection = 0;
+	}
 }
