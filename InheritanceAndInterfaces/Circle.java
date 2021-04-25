595648401daa63bc71728a7addc3e7dcb1827a7c
Yahya Hussein <yahyahussein91@gmail.com>
2021-04-24 19:10:00 -0700

    set up instance methods

--- InheritanceAndInterfaces/Circle.java
+++ InheritanceAndInterfaces/Circle.java
@@ -1,11 +1,19 @@
-public class Cylinder {
+public class Circle {
 	private double radius;	
 
-	public Cylinder(double radius){
+	public Circle(double radius){
 		if(radius < 0){
 			this.radius = 0;
 		}else {
 			this.radius = radius;
 		}
+	}
+
+	public double getRadius(){
+		return this.radius;
+	}
+
+	public double getArea(){
+		return (radius * radius) * Math.PI();
 	}
 }
