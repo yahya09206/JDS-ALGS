26867344d37013f21f5b88e9aeac7144ddd1de51
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-01 17:50:33 -0700

    getters for fields

--- InheritanceAndInterfaces/Case.java
+++ InheritanceAndInterfaces/Case.java
@@ -10,4 +10,24 @@ public class Case {
 		this.powerSupply = powerSupply;
 		this.dimensions = dimensions;
 	}
+
+	public void pressPowerButton(){
+		System.out.println("Power button pressed");
+	}
+
+	public String getModel(){
+		return this.model;
+	}
+
+	public String getManufacturer(){
+		return this.manufacturer;
+	}
+
+	public String getPowerSupply(){
+		return this.powerSupply;
+	}
+
+	public Dimensions getDimensions(){
+		return this.dimensions;
+	}
 }
