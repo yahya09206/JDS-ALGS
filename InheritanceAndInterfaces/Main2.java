3a166c90af2d1441d4c9d8fc70ef88cf47e1d909
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-01 18:05:48 -0700

    created instance to create pc

--- InheritanceAndInterfaces/Main2.java
+++ InheritanceAndInterfaces/Main2.java
@@ -7,5 +7,7 @@ public class Main2 {
 		Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
 
 		MotherBoard theMotherBoard = new MotherBoard("BJ-200", "Asus", 4, 6, "v2.44");
+
+		PC thePC = new PC(theCase, theMonitor, theMotherBoard);
 	}
 }
