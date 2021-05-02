dc145555988728ff834a5ea3c53ccaae745205a5
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-01 18:01:24 -0700

    fields constructor and getters for monitor class

--- InheritanceAndInterfaces/PC.java
+++ InheritanceAndInterfaces/PC.java
@@ -1,4 +1,23 @@
 public class PC {
 	private Case theCase;
-	private
+	private Monitor monitor;
+	private MotherBoard motherBoard;
+
+	public PC(Case case, Monitor monitor, MotherBoard motherBoard){
+		this.theCase = theCase;
+		this.monitor = monitor;
+		this.motherBoard = motherBoard;
+	}
+
+	public Case getTheCase(){
+		return this.theCase;
+	}
+
+	public Monitor getMonitor(){
+		return this.monitor;
+	}
+
+	public MotherBoard getMotherBoard(){
+		return this.motherBoard;
+	}
 }
