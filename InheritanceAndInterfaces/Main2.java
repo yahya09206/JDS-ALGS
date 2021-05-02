61d681999b25bfe4eb531efa80cda53fd348481c
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-01 18:04:02 -0700

    created instance of monitor class

--- InheritanceAndInterfaces/Main2.java
+++ InheritanceAndInterfaces/Main2.java
@@ -3,5 +3,7 @@ public class Main2 {
 	public static void main(String[] args){
 		Dimensions dimensions = new Dimensions(20, 20, 5);
 		Case theCase = new Case("220b", "Dell", "240", dimensions);
+
+		Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
 	}
 }
