39ddd6d2bb11a2ca398888342ccf11de94eb7b8c
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-01 18:33:46 -0700

    moved files to proper dirs

---
+++ CompisitionEncapsulationPolyMorphism/Main2.java
@@ -0,0 +1,18 @@
+//Will be covering composition, encapusation and interfaces
+public class Main2 {
+	public static void main(String[] args){
+		Dimensions dimensions = new Dimensions(20, 20, 5);
+		Case theCase = new Case("220b", "Dell", "240", dimensions);
+
+		Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
+
+		MotherBoard theMotherBoard = new MotherBoard("BJ-200", "Asus", 4, 6, "v2.44");
+
+		PC thePC = new PC(theCase, theMonitor, theMotherBoard);
+		// thePC.getMonitor().drawPixelAt(1500, 1200, "red");
+		// thePC.getMotherBoard().loadProgram("Windows 1.0");
+		// thePC.getTheCase().pressPowerButton();
+		thePC.powerUp();
+
+	}
+}
