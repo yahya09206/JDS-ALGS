9e12f6bcec434a9744813db12be4f8bcee937835
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-01 17:48:51 -0700

    created fields and constructor for case class

---
+++ InheritanceAndInterfaces/Case.java
@@ -0,0 +1,13 @@
+public class Case {
+	private String model;
+	private String manufacturer;
+	private String powerSupply;
+	private Dimensions dimensions;
+
+	public Case(String model, String manufacturer, String powerSupply, Dimensions dimensions){
+		this.model = model;
+		this.manufacturer = manufacturer;
+		this.powerSupply = powerSupply;
+		this.dimensions = dimensions;
+	}
+}
