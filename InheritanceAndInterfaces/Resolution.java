593857550fedfcc02e9ca482ccb0daab7bb29d33
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-01 17:46:55 -0700

    getters for fields

--- InheritanceAndInterfaces/Resolution.java
+++ InheritanceAndInterfaces/Resolution.java
@@ -3,6 +3,15 @@ public class Resolution {
 	private int height;
 
 	public Resolution(int width, int height){
-		
+		this.width = width;
+		this.height = height;
+	}
+
+	public int getWidth(){
+		return this.width;
+	}
+
+	public int getHeight(){
+		return this.height;
 	}
 }
