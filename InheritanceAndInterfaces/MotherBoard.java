b85f5b8fc66e8bc36952c0eb278556cb2e90cbee
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-01 17:37:19 -0700

    creating classes for compisition lesson

---
+++ InheritanceAndInterfaces/MotherBoard.java
@@ -0,0 +1,15 @@
+public class MotherBoard {
+	private String model;
+	private String manufacturer;
+	private int ramSlots;
+	private int cardSlots;
+	private String bios;
+
+	public MotherBoard(String model, String manufacturer, int ramSlots, int cardSlots, String bios){
+		this.model = model;
+		this.manufacturer = manufacturer;
+		this.ramSlots = ramSlots;
+		this.cardSlots = cardSlots;
+		this.bio = bios;
+	}
+}
