0240ad12539c7c2aa5708eb1390e468a218a031f
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-01 17:34:14 -0700

    starting on compisition

---
+++ InheritanceAndInterfaces/Car2.java
@@ -0,0 +1,10 @@
+public Car2 {
+	private int doors;
+	private int engineCapicity;
+
+	public Car2(String name, int doors, int engineCapicity){
+		super(name);
+		this.doors = doors;
+		this.engineCapicity = engineCapicity;
+	}
+}
---
+++ InheritanceAndInterfaces/Vehicle2.java
@@ -0,0 +1,7 @@
+public Vehicle2 {
+	private String name;
+
+	public Vehicle2(String name){
+		this.name = name;
+	}
+}
