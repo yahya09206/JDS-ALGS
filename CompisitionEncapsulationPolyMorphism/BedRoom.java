99f931b435f307bb59be1f1102893f65d5859742
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-05 18:09:57 -0700

    set up constructor

--- CompisitionEncapsulationPolyMorphism/BedRoom.java
+++ CompisitionEncapsulationPolyMorphism/BedRoom.java
@@ -8,6 +8,15 @@ public class BedRoom {
 	private Bed bed;
 	private Lamp lamp;
 
-	
+	public Bedroom(String name, Wall1 wall1, Wall2 wall2, Wall3 wall3, Wall4 wall4, Ceiling ceiling, Bed bed, Lamp lamp){
+		this.name = name;
+		this.wall1 = wall1;
+		this.wall2 = wall2;
+		this.wall3 = wall3;
+		this.wall4 = wall4;
+		this.ceiling = ceiling;
+		this.bed = bed;
+		this.lamp = lamp;
+	}
 
 }
