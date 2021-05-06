81908cb6ce7184d24266cf3b7545d7f0fb5f014c
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-05 18:31:46 -0700

    making instances of bedroom class

--- CompisitionEncapsulationPolyMorphism/Main.java
+++ CompisitionEncapsulationPolyMorphism/Main.java
@@ -5,5 +5,13 @@ public class Main {
 		Wall wall1 = new Wall("West");
 		Wall wall1 = new Wall("West");
 		Wall wall1 = new Wall("West");
+
+		Ceiling ceiling = new Ceiling(12, 55);
+
+		Bed bed = new Bed("Modern", 4. 3. 2. 1);
+
+		Lamp lamp = new Lamp("Class", false, 75);
+
+		Bedroom bedroom = new Bedroom("Tims", wall1, wall12, wall3, wall4, ceiling, bed, lamp);
 	}
 }
