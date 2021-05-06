093644cc2891b64d60ed8b17ffd8aa9f8d9ce851
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-05 18:24:56 -0700

    created main class to run code for challenge

---
+++ CompisitionEncapsulationPolyMorphism/Main.java
@@ -0,0 +1,9 @@
+//Client side for challenge
+public class Main {
+	public static void main(String[] args){
+		Wall wall1 = new Wall("West");
+		Wall wall1 = new Wall("West");
+		Wall wall1 = new Wall("West");
+		Wall wall1 = new Wall("West");
+	}
+}
