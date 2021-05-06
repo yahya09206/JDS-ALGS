7440d868ff198cdc6cf6002e21f92187cabd7826
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-05 18:14:09 -0700

    set up fields and contrcutor

---
+++ CompisitionEncapsulationPolyMorphism/Ceiling.java
@@ -0,0 +1,9 @@
+public class Ceiling {
+	private int height;
+	private int paintedColor;
+
+	public Ceiling(int height, int paintedColor){
+		this.height = height;
+		this.paintedColor = paintedColor;
+	}
+}
