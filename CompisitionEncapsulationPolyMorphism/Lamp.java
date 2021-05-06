24cb00c8310c882bef20e0b52064bfc924308a7d
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-05 18:21:16 -0700

    constructor for field class

--- CompisitionEncapsulationPolyMorphism/Lamp.java
+++ CompisitionEncapsulationPolyMorphism/Lamp.java
@@ -2,4 +2,12 @@ public class Lamp {
 	private String style;
 	private boolean battery;
 	private int globRating;
+
+	public Lamp(String style, boolean battery, int globRating){
+		this.style = style;
+		this.battery = battery;
+		this.globRating = globRating;
+	}
+
+	public void
 }
