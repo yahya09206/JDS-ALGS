df6472116c7f193526e46f398b70c1bdc8872826
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-06 18:40:32 -0700

    created classes inside the main method

--- CompisitionEncapsulationPolyMorphism/Main4.java
+++ CompisitionEncapsulationPolyMorphism/Main4.java
@@ -1,5 +1,17 @@
+class Movie {
+	private String name;
+
+	public Movie(String name){
+		this.name = name;
+	}
+
+	public String plot() {
+		return "No plot here";
+	}
+}
+
 public class Main4 {
 	public static void main(String[] args){
-		
+
 	}
 }
