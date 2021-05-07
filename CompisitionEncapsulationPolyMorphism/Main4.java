bd23becd99352f048bb044a31ccc718bcfcab9dd
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-06 18:47:51 -0700

    method to return random movie

--- CompisitionEncapsulationPolyMorphism/Main4.java
+++ CompisitionEncapsulationPolyMorphism/Main4.java
@@ -59,20 +59,37 @@ class Forgetable extends Movie {
 	}
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+//Main Method
 public class Main4 {
 	public static void main(String[] args){
 
 	}
+
+	public static Movie randomMovie(){
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
