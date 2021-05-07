717c12c15cca4229ed375017de3f889d983d9b84
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-06 17:56:07 -0700

    created constructor

--- CompisitionEncapsulationPolyMorphism/Printer.java
+++ CompisitionEncapsulationPolyMorphism/Printer.java
@@ -3,4 +3,10 @@ public class Printer {
 	private double tonerLevel;
 	private int numOfPages;
 	private boolean isDuplex;
+
+	public Printer(double tonerLevel, int numOfPages, boolean isDuplex){
+		this.tonerLevel = tonerLevel;
+		this.numOfPages = numOfPages;
+		this.isDuplex = isDuplex;
+	}
 }
