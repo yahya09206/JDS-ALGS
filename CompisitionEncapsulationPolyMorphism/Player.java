bac48c2b1176e250d4208f9d09793edea3272a68
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-06 17:35:26 -0700

    method for when player loses health

--- CompisitionEncapsulationPolyMorphism/Player.java
+++ CompisitionEncapsulationPolyMorphism/Player.java
@@ -1,5 +1,14 @@
 public class Player {
+
 	public String name;
-	public String health;
+	public int health;
 	public String weapon;
+
+	public void loseHealth(int damage){
+		this.health = this.health - damage;
+		if(this.health <= 0){
+			System.out.println("Player knocked out");
+		}
+	}
+
 }
