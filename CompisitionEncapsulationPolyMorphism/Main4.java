be405775d6250ec206d173efb43a0e36061e34d4
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-06 18:46:34 -0700

    set up subqenquent classes that extend from movie

--- CompisitionEncapsulationPolyMorphism/Main4.java
+++ CompisitionEncapsulationPolyMorphism/Main4.java
@@ -10,6 +10,67 @@ class Movie {
 	}
 }
 
+class Jaws extends Movie {
+	public Jaws() {
+		super("Jaws");
+	}
+
+	public String plot() {
+		return "A shark eats lots of people";
+	}
+}
+
+class IndependenceDay extends Movie {
+	public IndependenceDay(){
+		super("Independence Day");
+	}
+
+	@Override
+	public String plot(){
+		return "Aliens attempt to take over planet earth";
+	}
+}
+
+class MazeRunner extends Movie {
+	public MazeRunner(){
+		super("Maze Runner");
+	}
+
+	@Override
+	public String plot(){
+		return "Kids try and escape a maze.";
+	}
+}
+
+class StarWars extends Movie {
+	public StarWars(){
+		super("Star Wars");
+	}
+
+	@Override
+	public String plot(){
+		return "Imperial forces try to take over the universe";
+	}
+}
+
+class Forgetable extends Movie {
+	public Forgetable(){
+		super("Forgetable");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class Main4 {
 	public static void main(String[] args){
 
