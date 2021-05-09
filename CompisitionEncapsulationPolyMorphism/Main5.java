dfd285922a5720b18fb17af8c9bc381b65fbe610
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-08 18:41:09 -0700

    setting up car class

--- CompisitionEncapsulationPolyMorphism/Main5.java
+++ CompisitionEncapsulationPolyMorphism/Main5.java
@@ -1,5 +1,15 @@
+public class Car {
+
+	private int engine;
+	private int cylinders;
+	private String name;
+
+	
+}
+
+//Main Class
 public class Main5 {
 	public static void main(String[] args){
-		
+
 	}
 }
