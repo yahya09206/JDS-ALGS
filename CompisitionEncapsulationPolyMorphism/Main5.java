8e15f03f643c6f5875632faac97f0c9895dee9bd
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-08 18:51:26 -0700

    works so far

--- CompisitionEncapsulationPolyMorphism/Main5.java
+++ CompisitionEncapsulationPolyMorphism/Main5.java
@@ -1,4 +1,4 @@
-public class Car {
+class Car {
 
 	private boolean engine;
 	private int cylinders;
@@ -92,6 +92,16 @@ class Mitsubishi extends Car {
 //Main Class
 public class Main5 {
 	public static void main(String[] args){
+
+		Car car = new Car(8, "Base car");
+		System.out.println(car.startEngine());
+		System.out.println(car.accelerate());
+		System.out.println(car.brake());
+
+		Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRW 4WD");
+		System.out.println(mitsubishi.startEngine());
+		System.out.println(mitsubishi.accelerate());
+		System.out.println(mitsubishi.brake());
 
 	}
 }
