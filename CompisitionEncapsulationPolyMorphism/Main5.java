47dd4ad15f52a9f7ca7126720419f508d7c05673
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-08 18:48:31 -0700

    going to test if mitsubishi class works

--- CompisitionEncapsulationPolyMorphism/Main5.java
+++ CompisitionEncapsulationPolyMorphism/Main5.java
@@ -37,7 +37,57 @@ class Mitsubishi extends Car {
 	public Mitsubishi(int cylinders, String name){
 		super(cylinders, name);
 	}
+
+	@Override
+	public String startEngine(){
+		return "Mitsubishi -> startEngine()";
+	}
+
+	@Override
+	public String accelerate(){
+		return "Mitsubishi -> accelerate()";
+	}
+
+	@Override
+	public String brake(){
+		return "Mitsubishi -> brake()";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Main Class
 public class Main5 {
