3bec7f90a5ec482cab79b5a2367513245188d450
Yahya Hussein <yahyahussein91@gmail.com>
2021-05-08 18:55:34 -0700

    added another class to extend from original car class

--- CompisitionEncapsulationPolyMorphism/Main5.java
+++ CompisitionEncapsulationPolyMorphism/Main5.java
@@ -75,38 +75,26 @@ class Ford extends Car {
 	}
 }
 
+class Holden extends Car {
+	public Holden(int cylinders, String name){
+		super(cylinders, name);
+	}
 
+	@Override
+	public String startEngine(){
+		return "Holden -> startEngine()";
+	}
 
+	@Override
+	public String accelerate(){
+		return "Holden -> accelerate()";
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	@Override
+	public String brake(){
+		return "Holden -> brake()";
+	}
+}
 
 //Main Class
 public class Main5 {
