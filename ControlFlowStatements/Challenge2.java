d55c5764917fdbb310b290f98c19cd8b2dd50c3b
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-22 20:00:16 -0700

    done with challenge 2

--- ControlFlowStatements/Challenge2.java
+++ ControlFlowStatements/Challenge2.java
@@ -1,5 +1,18 @@
 public class Challenge2 {
 	public static void main(String[] args){
-		
+
+		int sum = 0;
+		int counter = 0;
+		for(int i = 1; i <= 1000; i++){
+			if(i % 3 == 0 && i % 5 == 0){
+				sum += i;
+				counter++;
+				if(counter == 5){
+					System.out.println("Exiting for loop");
+					break;
+				}
+			}
+		}
+		System.out.println("Sum of all divisible by 3 and 5 are " + sum);
 	}
 }
