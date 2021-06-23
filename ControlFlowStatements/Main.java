29d3aa395faa7c90a9e3a03e88bd5ef51245158c
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-22 19:10:00 -0700

    calculating interest without for loop

--- ControlFlowStatements/Main.java
+++ ControlFlowStatements/Main.java
@@ -1,5 +1,14 @@
 public class Main {
 	public static void main(String[] args){
-		
+
+		System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+
+		for(){
+			
+		}
+	}
+
+	public static double calculateInterest(double amount, double interestRate) {
+		return(amount * (interestRate/100));
 	}
 }
