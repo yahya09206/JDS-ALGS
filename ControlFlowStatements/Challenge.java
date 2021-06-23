971b760917d2e8be0b9d8a2b4e99d00f1447dc82
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-22 19:43:53 -0700

    im an idiot

--- ControlFlowStatements/Challenge.java
+++ ControlFlowStatements/Challenge.java
@@ -1,6 +1,17 @@
 public class Challenge {
 	public static void main(String[] args){
 
+		int count = 0;
+		for(int i = 10; i < 50; i++){
+			if(isPrime(i)){
+				count++;
+				System.out.println("Number " + i + " is prime");
+				if(count == 3){
+					System.out.println("Exiting for loop");
+					break;
+				}
+			}
+		}
 	}
 
 	public static boolean isPrime(int n){
@@ -8,7 +19,7 @@ public class Challenge {
 			return false;
 		}
 		for(int i = 2; i <= n/2; i++){
-			if(n % 1 == 0){
+			if(n % i == 0){
 				return false;
 			}
 		}
