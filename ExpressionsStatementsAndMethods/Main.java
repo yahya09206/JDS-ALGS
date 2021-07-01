5e0d2f39bf6443310e3e698531b8a4289e574dd4
Yahya Hussein <yahyahussein91@gmail.com>
2021-06-30 19:35:29 -0700

    done with expressions

--- ExpressionsStatementsAndMethods/Main.java
+++ ExpressionsStatementsAndMethods/Main.java
@@ -1,5 +1,15 @@
 public class Main {
 	public static void main(String[] args){
-		
+
+		int int2 = 5;
+		//A mile is equal to 1.609344 kilometers
+		double kilometers = (100 * 1.609344);
+		int highScore = 50;
+
+		if(highScore == 50){
+			System.out.println("This is an expression"){
+				
+			}
+		}
 	}
 }
